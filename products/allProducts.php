de7036e95f40c9fffdfa09326cf4bc6a49605672
adhiraj1212 <np03cs4s220190@heraldcollege.edu.np>
2023-06-26 18:19:44 +0545

    All Products Minor Modification

--- products/allProducts.php
+++ products/allProducts.php
@@ -1,29 +1,5 @@
 <?php
 
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json");
-// header("Access-Control-Allow-Method: GET");
-// header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Method, Authorization, X-Requested-With");
-
-// include('function.php');
-
-// $requestMethod = $_SERVER["REQUEST_METHOD"];
-
-// if($requestMethod == "GET"){
-//     $vendor_id = isset($_GET['vendor_id']) ? $_GET['vendor_id'] : null;
-//     $productList = getProductList($vendor_id);
-//     echo $productList;
-// }
-// else
-// {
-//     $data=[
-//         'status' => 405,
-//         'message' => $requestMethod . ' Method Not Allowed',
-//     ];
-//     header("HTTP/1.0 405 Method Not Allowed");
-//     echo json_encode($data);
-// }
-
 require '../inc/dbcon.php';
 require '../vendor/autoload.php';
 use \Firebase\JWT\JWT;
@@ -62,7 +38,12 @@ if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
                 while($row = $result->fetch_assoc()) {
                     $products[] = $row;
                 }
-                echo json_encode($products);
+                // echo json_encode($products);
+                echo json_encode([
+                    "status" => 200,
+                "message" => "Get all products",
+                "data"=> $products
+                ]);
             } else {
                 echo json_encode([]);
             }
